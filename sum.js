dc817422012cf3237b25908351b7485a3d7b007e
akash makkpress <akash@makkpress.com>
2023-09-03 22:04:34 +0530

    commiting 2 files sum & minus

--- sum.js
+++ sum.js
@@ -2,5 +2,5 @@ function addNumbers(number1, number2) {
   return number1 + number2;
 }
 
-const result = addNumbers(5, 3); // This will result in result = 8
+const result = addNumbers(5, 9); // This will result in result = 8
 console.log(result); // Output will be 8
