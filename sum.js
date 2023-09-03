730224533db808ae4f76ce6c06ff71d0db82b01b
akash makkpress <akash@makkpress.com>
2023-09-03 21:56:16 +0530

    fisrt commit

---
+++ sum.js
@@ -0,0 +1,6 @@
+function addNumbers(number1, number2) {
+  return number1 + number2;
+}
+
+const result = addNumbers(5, 3); // This will result in result = 8
+console.log(result); // Output will be 8
